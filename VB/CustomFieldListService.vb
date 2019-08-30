332037d50a37cf0fdf3aae2eb11a13400da69eb0
DevExpressExampleBot <fakeEmail>
2019-08-30 16:35:24 +0300

    Source auto update [skip ci]

---
+++ VB/CustomFieldListService.vb
@@ -0,0 +1,31 @@
+﻿Imports System.Drawing
+Imports System.ComponentModel
+Imports DevExpress.Utils
+Imports DevExpress.XtraReports.UserDesigner
+Imports DevExpress.Utils.Svg
+
+Namespace FieldListCustomIcons
+
+	Friend Class CustomColumnImageProvider
+		Inherits DevExpress.Data.Browsing.Design.ColumnImageProvider
+
+		Private categoryNameIndex As Integer
+
+		Public Overrides Function GetColumnImageIndex(ByVal [property] As PropertyDescriptor, ByVal specifics As DevExpress.Data.Browsing.Design.TypeSpecifics) As Integer
+			If [property].Name.Equals("CategoryName") Then
+				Return categoryNameIndex
+			End If
+			Return MyBase.GetColumnImageIndex([property], specifics)
+		End Function
+		Public Overrides Function CreateSvgImageCollection() As SvgImageCollection
+			Dim result As SvgImageCollection = MyBase.CreateSvgImageCollection()
+			Dim image As New SvgImage(GetType(CustomColumnImageProvider), "FieldListCustomIcons.StarIcon.svg")
+
+			result.Add(image)
+			categoryNameIndex = result.Count - 1
+
+			Return result
+		End Function
+
+	End Class
+End Namespace
---
+++ VB/Program.vb
@@ -0,0 +1,15 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Windows.Forms
+
+Namespace FieldListCustomIcons
+	Friend Module Program
+
+		<STAThread>
+		Sub Main()
+			Application.EnableVisualStyles()
+			Application.SetCompatibleTextRenderingDefault(False)
+			Application.Run(New Form1())
+		End Sub
+	End Module
+End Namespace
